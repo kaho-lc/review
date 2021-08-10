5cea33e3306bcf8216fc2cdcfd8a23b8263b1bfc
lc <lc@163.com>
2021-08-10 09:51:50 +0800

    test commit

---
+++ Scala/src/main/scala/Demo01.scala
@@ -0,0 +1,17 @@
+/**
+ * @author kaho-lc
+ * @email lc1536328699@163.com
+ * @date 2021-08-09
+ * @time 19:53
+ */
+object Demo01 {
+
+  def main(args: Array[String]): Unit = {
+    println("hello world!")
+
+    for (i <- 1 to 10) {
+      println(i)
+    }
+
+  }
+}
